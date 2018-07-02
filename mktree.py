2934fc7a26f2e344a9086e215b05263a1a9f313b
KageShiron <KageShiron@gmail.com>
2018-07-02 23:08:19 +0900

    Fix: mktree.py and bbs.css

--- mktree.py
+++ mktree.py
@@ -1,16 +1,21 @@
 import csv
 import locale
+import sys
+import os
 from datetime import datetime as dt
 locale.setlocale(locale.LC_ALL, '')
 
 
+def usage():
+    print("Usage: python3 mktree.py /path/to/tsvfile.tsv /path/to/resultdir/")
+    exit()
+
+
 def makeSide(x):
     t = dt.strptime(x["time"], "%Y%m%d%w%H%M%S")
-    s = f"""<li>
-    <span class="no">[{x['id']}]</span>
-    <a class="title" href="#{x['id']}">{x['title']}</a>
-    <span class="author">{x['name']}</span>
-    <time datetime="{t.isoformat()}">{t.strftime("%Y年%m月%d日 %H:%M")}</time>
+    s = f"""<li><div class="list-title">
+    <span class="no">{x['id']}</span>
+    <a class="title" href="#{x['id']}">{x['title']}</a></div>
     """
     if "children" in x:
         s += "<ul>"
@@ -21,49 +26,63 @@ def makeSide(x):
     return s
 
 
-def writeBody(f, x, ind):
-    f.write("<li><section>")
+def makeBody(x):
     t = dt.strptime(x["time"], "%Y%m%d%w%H%M%S")
-    f.write('<h1 id="%s"><a href="#%s">%s</a><span class="title">%s</span><span class="author">%s</span><time datetime="%s">%s</time></h1>'
-            % (x["id"], x["id"], x["id"], x["title"], x["name"], t.isoformat(), t.strftime("%Y年%m月%d日 %H:%M")))
-    f.write('<div class="body">' + x["body"] + '</div>')
-    f.write("</section><ul>")
+    s = f"""<li id={x['id']}><section><h1>
+    <span class="no">[{x['id']}]</span>
+    <a class="title" href="#{x['id']}">{x['title']}</a>
+    <span class="author">{x['name']}</span>
+    <time datetime="{t.isoformat()}">{t.strftime("%Y年%m月%d日 %H:%M")}</time></h1>
+    <div class="body">{x['body']}</div></section>
+    """
     if "children" in x:
+        s += "<ul>"
         for c in x["children"]:
-            writeBody(f, c, ind+1)
-    f.write("</ul></li>")
+            s += makeBody(c)
+        s += "</ul>"
+    s += "</li>"
+    return s
 
 
 def writeheader(f, x):
     f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><title>" +
-            x['title']+"</title><link rel='stylesheet' href='bbs.css' /><body><ul>")
+            x['title']+"</title><link rel='stylesheet' href='bbs.css' /><body>")
 
 
 def writefooter(f,):
-    f.write('</ul></body></html>')
+    f.write('</body></html>')
 
 
-with open("data.tsv", mode="r", encoding="utf-8", newline="") as tsv:
-    # TSV reader
+if __name__ == '__main__':
 
-    ls = list(csv.DictReader(tsv, delimiter="\t"))
+    if len(sys.argv) < 3:
+        usage()
+    result = sys.argv[2]
 
-    # create tree structure
-    for x in ls:
-        for y in ls:
-            if y["parent"] == x["id"]:
-                if "children" in x:
-                    x["children"] += [y]
-                else:
-                    x["children"] = [y]  # new list
-                y["noroot"] = True
+    if not os.path.exists(result):
+        os.makedirs(result)
 
-    # write html
-    for x in ls:
-        if "noroot" not in x:   # only root node
-            print(x['id'])
-            with open('result/' + x['id'] + '.html', "w", encoding="utf-8") as f:
-                writeheader(f, x)
-                f.write(makeSide(x))
-                writeBody(f, x, 0)
-                writefooter(f)
+    with open(sys.argv[1], mode="r", encoding="utf-8", newline="") as tsv:
+        # TSV reader
+
+        ls = list(csv.DictReader(tsv, delimiter="\t"))
+
+        # create tree structure
+        for x in ls:
+            for y in ls:
+                if y["parent"] == x["id"]:
+                    if "children" in x:
+                        x["children"] += [y]
+                    else:
+                        x["children"] = [y]  # new list
+                    y["noroot"] = True
+
+        # write html
+        for x in ls:
+            if "noroot" not in x:   # only root node
+                print(x['id'])
+                with open(result + "/" + x['id'] + '.html', "w", encoding="utf-8") as f:
+                    writeheader(f, x)
+                    f.write(f'<ul class="side">{makeSide(x)}</ul>')
+                    f.write(f'<ul class="main">{makeBody(x)}</ul>')
+                    writefooter(f)
